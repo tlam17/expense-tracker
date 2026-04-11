400225952b682fe3de3f02d1176a565ac6c7e6e2
tlam17 <94070835+tlam17@users.noreply.github.com>
2026-04-11 09:26:02 -0700

    Add unit tests for getCategories in CategoryController

--- backend/src/test/java/com/tylerlam/expense_tracker/category/CategoryControllerTest.java
+++ backend/src/test/java/com/tylerlam/expense_tracker/category/CategoryControllerTest.java
@@ -2,21 +2,30 @@ package com.tylerlam.expense_tracker.category;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
-import org.springframework.http.MediaType;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(CategoryController.class)
 public class CategoryControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
     @Test
-    public void getCategories() throws Exception {
-        
+    public void getCategories_returns200() throws Exception {
+      mvc.perform(get("/api/categories"))
+                .andExpect(status().isOk());
     }
 
+    @Test
+    public void getCategories_returnsExpectedCategories() throws Exception {
+        mvc.perform(get("/api/categories"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$[0]").value("Food"))
+            .andExpect(jsonPath("$[1]").value("Rent"))
+            .andExpect(jsonPath("$[2]").value("Utilities"));
+    }
 }
