c42f4fab1afae82e96e50a815c88b2386a6eea06
tlam17 <94070835+tlam17@users.noreply.github.com>
2026-04-25 14:28:19 -0500

    Add @TestPropertySource

--- backend/src/test/java/com/tylerlam/expense_tracker/ExpenseTrackerApplicationTests.java
+++ backend/src/test/java/com/tylerlam/expense_tracker/ExpenseTrackerApplicationTests.java
@@ -2,8 +2,16 @@ package com.tylerlam.expensetracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.autoconfigure.exclude=" +
+    "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+    "org.springframework.boot.flyway.autoconfigure.FlywayAutoConfiguration," +
+    "org.springframework.boot.orm.jpa.hibernate.SpringJtaPlatform," +
+    "org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration"
+})
 class ExpenseTrackerApplicationTests {
 
 	@Test
